b11f1da0424ecd268ac725356feaa8c0c3f0641f
AlemSnyder <44094752+AlemSnyder@users.noreply.github.com>
2022-07-21 07:55:35 -0500

    deleted comments

--- src/GUI/renderer.hpp
+++ src/GUI/renderer.hpp
@@ -64,46 +64,6 @@ public:
         depth_view_matrix_ =
             glm::lookAt(light_direction_, glm::vec3(0, 0, 0), glm::vec3(0, 1, 0));
 
-        // ---------------------------------------------
-        // Render Shadow to Texture - specific code begins here
-        // ---------------------------------------------
-
-        // The framebuffer, which regroups 0, 1, or more textures, and 0 or 1 depth
-        // buffer.
-        //GLuint FramebufferName = 0;
-        //glGenFramebuffers(1, &FramebufferName);
-        //glBindFramebuffer(GL_FRAMEBUFFER, FramebufferName);
-
-        // Depth texture. Slower than a depth buffer, but you can sample it later in
-        // your shader
-        /*GLuint depthTexture;
-        glGenTextures(1, &depthTexture);
-        glBindTexture(GL_TEXTURE_2D, depthTexture);
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT16, 1024 * 4, 1024 * 4, 0,
-                    GL_DEPTH_COMPONENT, GL_FLOAT, 0);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_COMPARE_FUNC, GL_LEQUAL);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_COMPARE_MODE,
-                        GL_COMPARE_R_TO_TEXTURE);*/
-
-        //glFramebufferTexture(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, depthTexture, 0);
-
-        // No color output in the bound framebuffer, only depth.
-        //glDrawBuffer(GL_NONE);
-
-        // Always check that our framebuffer is ok
-        //if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
-            //return;
-
-        // The quad's FBO. Used only for visualizing the shadow map.
-        //static const GLfloat g_quad_vertex_buffer_data[] = {
-        //    -1.0f, -1.0f, 0.0f, 1.0f, -1.0f, 0.0f, -1.0f, 1.0f, 0.0f,
-        //    -1.0f, 1.0f,  0.0f, 1.0f, -1.0f, 0.0f, 1.0f,  1.0f, 0.0f,
-        //};
-
     }
 
     ~MainRenderer(){
@@ -204,7 +164,7 @@ public:
                               (void *)0 // array buffer offset
         );
 
-        // 2nd attribute buffer : colorss
+        // 2nd attribute buffer : colors
         glEnableVertexAttribArray(1);
         glBindBuffer(GL_ARRAY_BUFFER, terrain_mesh_.get_color_buffer());
         glVertexAttribPointer(1,        // attribute
