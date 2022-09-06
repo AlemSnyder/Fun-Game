0d6d55c1ef88401413b0abb81bee2f67135a8102
Nino Maruszewski <nino.maruszewski@gmail.com>
2022-09-06 14:35:09 -0500

    perf: path caching

--- src/util/files.cpp
+++ src/util/files.cpp
@@ -11,15 +11,21 @@ namespace files {
 
 std::filesystem::path
 get_root_path() {
-    int length = wai_getExecutablePath(NULL, 0, NULL);
-    std::string dir_path = std::string(length + 1, '\0');
+    static std::filesystem::path root_path;
+    if (root_path.empty()) {
+        // Not cached
+        int length = wai_getExecutablePath(nullptr, 0, nullptr);
+        std::string dir_path = std::string(length + 1, '\0');
 
-    int dirname_length;
-    wai_getExecutablePath(dir_path.data(), length, &dirname_length);
-    dir_path.resize(dirname_length);  // remove the executable name
+        int dirname_length;
+        wai_getExecutablePath(dir_path.data(), length, &dirname_length);
+        dir_path.resize(dirname_length); // remove the executable name
 
-    std::filesystem::path root_path = std::filesystem::path(dir_path) / ROOT_DIR_PATH;
-    return std::filesystem::absolute(root_path);
+        root_path =
+            std::filesystem::absolute(std::filesystem::path(dir_path) / ROOT_DIR_PATH);
+    }
+
+    return root_path;
 }
 
 } // namespace files
