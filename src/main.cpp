6c52943fe6870ac671fc440ea98739686df50672
AlemSnyder <44094752+AlemSnyder@users.noreply.github.com>
2022-12-03 12:46:35 -0800

    add quill logger

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@
 #include "util/files.hpp"
 #include "world.hpp"
 
+#include <quill/Quill.h>
 #include <argh.h>
 
 #include <GL/glew.h>
@@ -194,6 +195,35 @@ GUITest(const std::filesystem::path path) {
     return GUITest(path.string());
 }
 
+inline int
+LogTest(){
+    //quill::enable_console_colours();
+    // Does not work a quill config object is required
+    quill::Config cfg;
+    cfg.enable_console_colours = true;
+    quill::configure(cfg);
+    quill::start();
+
+    quill::Logger* logger = quill::get_logger();
+    logger->set_log_level(quill::LogLevel::TraceL3);
+
+    // enable a backtrace that will get flushed when we log CRITICAL
+    logger->init_backtrace(2, quill::LogLevel::Critical);
+
+    LOG_BACKTRACE(logger, "Backtrace log {}", 1);
+    LOG_BACKTRACE(logger, "Backtrace log {}", 2);
+
+    LOG_INFO(logger, "Welcome to Quill!");
+    LOG_ERROR(logger, "An error message. error code {}", 123);
+    LOG_WARNING(logger, "A warning message.");
+    LOG_CRITICAL(logger, "A critical error.");
+    LOG_DEBUG(logger, "Debugging foo {}", 1234);
+    LOG_TRACE_L1(logger, "{:>30}", "right aligned");
+    LOG_TRACE_L2(logger, "Positional arguments are {1} {0} ", "too", "supported");
+    LOG_TRACE_L3(logger, "Support for floats {:03.2f}", 1.23456);
+    return 0;
+}
+
 int
 main(int argc, char** argv) {
     argh::parser cmdl;
@@ -253,5 +283,7 @@ main(int argc, char** argv) {
         return path_finder_test(path_in, path_out);
     } else if (run_function == "GUITest") {
         return GUITest(path_in);
+    } else if (run_function == "Logging") {
+        return LogTest();
     }
 }
