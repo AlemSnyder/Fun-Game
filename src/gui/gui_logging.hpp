378d68f440d5241f1af06335201556f15d57182f
AlemSnyder <44094752+AlemSnyder@users.noreply.github.com>
2022-12-28 10:57:24 -0500

    changed format, and comments

--- src/gui/gui_logging.hpp
+++ src/gui/gui_logging.hpp
@@ -9,7 +9,9 @@
 namespace gui {
 
 // I decided not to do do this line by line because
-// defines are hare, and I am lazy.
+// defines are hard, and I am lazy.
+// (if were are going to do this line by line, then we should get the line in the log
+// file) We are using a gl call back on all messages.
 void GLAPIENTRY
 MessageCallback(
     GLenum source, GLenum type, unsigned int id, GLenum severity, GLsizei length,
@@ -21,58 +23,28 @@ MessageCallback(
 
     std::string source_sting;
     std::string type_string;
-
+    // clang-format off
     switch (source) {
-        case GL_DEBUG_SOURCE_API:
-            source_sting = "API";
-            break;
-        case GL_DEBUG_SOURCE_WINDOW_SYSTEM:
-            source_sting = "Window System";
-            break;
-        case GL_DEBUG_SOURCE_SHADER_COMPILER:
-            source_sting = "Shader Compiler";
-            break;
-        case GL_DEBUG_SOURCE_THIRD_PARTY:
-            source_sting = "Third Party";
-            break;
-        case GL_DEBUG_SOURCE_APPLICATION:
-            source_sting = "Application";
-            break;
-        case GL_DEBUG_SOURCE_OTHER:
-            source_sting = "Other";
-            break;
+        case GL_DEBUG_SOURCE_API:               source_sting = "API";               break;
+        case GL_DEBUG_SOURCE_WINDOW_SYSTEM:     source_sting = "Window System";     break;
+        case GL_DEBUG_SOURCE_SHADER_COMPILER:   source_sting = "Shader Compiler";   break;
+        case GL_DEBUG_SOURCE_THIRD_PARTY:       source_sting = "Third Party";       break;
+        case GL_DEBUG_SOURCE_APPLICATION:       source_sting = "Application";       break;
+        case GL_DEBUG_SOURCE_OTHER:             source_sting = "Other";             break;
     }
 
     switch (type) {
-        case GL_DEBUG_TYPE_ERROR:
-            type_string = "Error";
-            break;
-        case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR:
-            type_string = "Deprecated Behaviour";
-            break;
-        case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR:
-            type_string = "Undefined Behaviour";
-            break;
-        case GL_DEBUG_TYPE_PORTABILITY:
-            type_string = "Portability";
-            break;
-        case GL_DEBUG_TYPE_PERFORMANCE:
-            type_string = "Performance";
-            break;
-        case GL_DEBUG_TYPE_MARKER:
-            type_string = "Marker";
-            break;
-        case GL_DEBUG_TYPE_PUSH_GROUP:
-            type_string = "Push Group";
-            break;
-        case GL_DEBUG_TYPE_POP_GROUP:
-            type_string = "Pop Group";
-            break;
-        case GL_DEBUG_TYPE_OTHER:
-            type_string = "Other";
-            break;
+        case GL_DEBUG_TYPE_ERROR:               type_string = "Error";                  break;
+        case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR: type_string = "Deprecated Behaviour";   break;
+        case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR:  type_string = "Undefined Behaviour";    break;
+        case GL_DEBUG_TYPE_PORTABILITY:         type_string = "Portability";            break;
+        case GL_DEBUG_TYPE_PERFORMANCE:         type_string = "Performance";            break;
+        case GL_DEBUG_TYPE_MARKER:              type_string = "Marker";                 break;
+        case GL_DEBUG_TYPE_PUSH_GROUP:          type_string = "Push Group";             break;
+        case GL_DEBUG_TYPE_POP_GROUP:           type_string = "Pop Group";              break;
+        case GL_DEBUG_TYPE_OTHER:               type_string = "Other";                  break;
     }
-
+    //clang-format on
     switch (severity) {
         case GL_DEBUG_SEVERITY_HIGH:
             LOG_ERROR(
