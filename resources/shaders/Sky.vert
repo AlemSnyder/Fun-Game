12aea07ee97e59f44b7a507e12a1aa5929fd2555
AlemSnyder <44094752+AlemSnyder@users.noreply.github.com>
2023-04-10 15:34:23 -0700

    stars work

--- resources/shaders/Sky.vert
+++ resources/shaders/Sky.vert
@@ -5,7 +5,7 @@ layout(location = 0) in vec4 pos;
 layout(location = 1) in float age_in;
 layout(location = 2) in vec2 star_corner;
 uniform mat4 MVP;
-//uniform mat4 pixel_projection;
+uniform mat4 pixel_projection;
 
 // Output data ; will be interpolated for each fragment.
 out float age;
@@ -14,7 +14,9 @@ void
 main() {
     vec4 star_center_camera_space = MVP * vec4(pos.xyz, 0);
 
-    gl_Position = star_center_camera_space + vec4(star_corner * pos.w/100,0,1);
+    //star_center_camera_space = star_center_camera_space / star_center_camera_space.w
+
+    gl_Position = vec4(star_center_camera_space.xy / star_center_camera_space.w + (pixel_projection * vec4(star_corner * pos.w, 0, 0)).xy, 9, 1);
 
     //gl_Position = star_center_camera_space;//MVP * vec4(star_corner*100,-1,1);
 
--- src/gui/render/sky.cpp
+++ src/gui/render/sky.cpp
@@ -2,10 +2,9 @@
 
 #include "../../util/files.hpp"
 #include "../controls.hpp"
+#include "../data_structures/sky_data.hpp"
 #include "../meshloader.hpp"
 #include "../shader.hpp"
-#include "../data_structures/sky_data.hpp"
-#include "../../util/files.hpp"
 
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
@@ -17,8 +16,7 @@ namespace gui {
 
 namespace sky {
 
-SkyRenderer::SkyRenderer() : sky_data_(files::get_data_path() / "stars.json"){
-
+SkyRenderer::SkyRenderer() : sky_data_(files::get_data_path() / "stars.json") {
     programID_ = load_shaders(
         files::get_resources_path() / "shaders" / "Sky.vert",
         files::get_resources_path() / "shaders" / "Sky.frag"
@@ -26,9 +24,9 @@ SkyRenderer::SkyRenderer() : sky_data_(files::get_data_path() / "stars.json"){
     // ---- set uniforms ----
     matrix_view_projection_ID_ = glGetUniformLocation(programID_, "MVP");
     pixel_matrix_ID_ = glGetUniformLocation(programID_, "pixel_projection");
-    //star_positions_ID_ = glGetUniformLocation(programID_, "star_positions");
-    //star_brightness_ID_ = glGetUniformLocation(programID_, "star_brightness");
-    //star_shape_ID_ = glGetUniformLocation(programID_, "star_shape");
+    // star_positions_ID_ = glGetUniformLocation(programID_, "star_positions");
+    // star_brightness_ID_ = glGetUniformLocation(programID_, "star_brightness");
+    // star_shape_ID_ = glGetUniformLocation(programID_, "star_shape");
 }
 
 SkyRenderer::~SkyRenderer() {
@@ -37,7 +35,6 @@ SkyRenderer::~SkyRenderer() {
 
 void
 SkyRenderer::render(GLFWwindow* window) const {
-
     glBindFramebuffer(GL_FRAMEBUFFER, 0);
 
     glDisable(GL_CULL_FACE);
@@ -51,10 +48,23 @@ SkyRenderer::render(GLFWwindow* window) const {
     glUseProgram(programID_);
 
     // Compute the MVP matrix from keyboard and mouse input
-    glm::mat4 pixel_window = {1/width,0,0,0,
-                            0, 1/height,0,0,
-                            0,0,1,0,
-                            0,0,0,1};
+    glm::mat4 pixel_window = {
+        1.0 / static_cast<float>(width),
+        0,
+        0,
+        0,
+        0,
+        1 / static_cast<float>(height),
+        0,
+        0,
+        0,
+        0,
+        1,
+        0,
+        0,
+        0,
+        0,
+        1};
     glm::mat4 projection_matrix = controls::get_projection_matrix();
     glm::mat4 view_matrix = controls::get_view_matrix();
     glm::mat4 MVP = projection_matrix * view_matrix; // Model View Projection
@@ -64,12 +74,12 @@ SkyRenderer::render(GLFWwindow* window) const {
     glUniformMatrix4fv(matrix_view_projection_ID_, 1, GL_FALSE, &MVP[0][0]);
     glUniformMatrix4fv(pixel_matrix_ID_, 1, GL_FALSE, &pixel_window[0][0]);
 
-    //glUniform1i(star_num_ID_, sky_data_.get_num_stars());
+    // glUniform1i(star_num_ID_, sky_data_.get_num_stars());
 
     // Bind Shadow Texture to Texture Unit 1
-    //glActiveTexture(GL_TEXTURE1);
-    //glBindTexture(GL_TEXTURE_1D, sky_data_.get_stars_texture());
-    //glUniform1i(star_texture_, 1);
+    // glActiveTexture(GL_TEXTURE1);
+    // glBindTexture(GL_TEXTURE_1D, sky_data_.get_stars_texture());
+    // glUniform1i(star_texture_, 1);
 
     // 1st attribute buffer : vertices
     glEnableVertexAttribArray(0);
@@ -111,13 +121,13 @@ SkyRenderer::render(GLFWwindow* window) const {
     glVertexAttribDivisor(1, 1);
 
     // Index buffer
-    //glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, sky_data_.get_element_buffer());
+    // glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, sky_data_.get_element_buffer());
 
     // Draw the triangles !
     glDrawArraysInstanced(
-        GL_TRIANGLE_STRIP,               // mode
-        0,//4,//sky_data_.get_star_shape(),       // count
-        4,//GL_UNSIGNED_SHORT,               // type
+        GL_TRIANGLE_STRIP, // mode
+        0,                 // 4,//sky_data_.get_star_shape(),       // count
+        4,                 // GL_UNSIGNED_SHORT,               // type
         //(void*)0,                        // element array buffer offset
         sky_data_.get_num_stars()
 
@@ -128,7 +138,7 @@ SkyRenderer::render(GLFWwindow* window) const {
     glDisableVertexAttribArray(0);
     glDisableVertexAttribArray(1);
     glDisableVertexAttribArray(2);
-    
+
     // Use our shader
     glUseProgram(programID_);
 }
