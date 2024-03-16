163dc699ebeccfa15af23894f7a624675b40394a
Alem Snyder <alemsnyder@gmail.com>
2024-03-15 22:01:42 -0700

    fix bug in terrain mesh generation

--- src/world/world.cpp
+++ src/world/world.cpp
@@ -168,9 +168,7 @@ World::update_all_chunks_mesh() {
         chunks_mesh_.clear();
         chunks_mesh_.reserve(num_chunks);
         for (size_t i = 0; i < num_chunks; i++) {
-            chunks_mesh_.push_back(std::make_shared<gui::gpu_data::TerrainMesh>(
-                gui::gpu_data::TerrainMesh()
-            ));
+            chunks_mesh_.push_back(std::make_shared<gui::gpu_data::TerrainMesh>());
         }
     }
 
