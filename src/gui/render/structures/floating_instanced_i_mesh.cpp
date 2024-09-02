05790415916384aef93861e7914c74517cb663ea
Alem Snyder <alemsnyder@gmail.com>
2024-09-02 14:24:27 -0500

    initialize num_models

--- src/gui/render/structures/floating_instanced_i_mesh.cpp
+++ src/gui/render/structures/floating_instanced_i_mesh.cpp
@@ -17,7 +17,8 @@ FloatingInstancedIMeshGPU::FloatingInstancedIMeshGPU(
     const world::entity::Mesh& mesh, const std::vector<glm::mat4>& model_transforms
 ) :
     IMeshGPU(mesh),
-    transforms_array_(model_transforms, 1) {
+    transforms_array_(model_transforms, 1),
+    num_models_(model_transforms.size()) {
     // InstancedInt does not have a color texture. One must inherit from this
     // class and define a method that creates a color texture, and sets its id
     // as color_texture_.
