fdca8c6a24318a823814dbcfdd7c6786e303a6ad
Alem Snyder <alemsnyder@gmail.com>
2025-06-23 11:27:18 -0700

    formations of a plan

--- data/Base/models/Test_Entity/ai.lua
+++ data/Base/models/Test_Entity/ai.lua
@@ -6,3 +6,19 @@ function move_direction(this)
     end
     return {0, 0, 0}
 end
+
+function plan()
+    plan_table
+    path = PathFinder:find("base/Flower_Test")
+    if path["len"] > 0 then
+        plan_table["follow"] = path
+        plan_table["action"] = "destroy"
+        return plan_table
+    end
+    return nil
+end
+
+-- ai.lua file needs a number of functions
+--   plan a series of paths and actions with a priority
+--     need set of actions "pickup", "destroy", "place" etc
+--   needs to know when to stop the plan
